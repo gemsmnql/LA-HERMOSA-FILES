aaa5b6df16684816699e69506bee62ac1eb7b305
Cabe <150228018+Cabiiii@users.noreply.github.com>
2026-02-11 23:20:23 +0800

    Update blog.ts

--- frontend/src/app/services/blog.ts
+++ frontend/src/app/services/blog.ts
@@ -1,20 +1,19 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:3000/api/blogs';
+  private apiUrl = `${environment.apiUrl}/api/blogs`;
 
-  // Get all blogs
   getBlogs(): Observable<any[]> {
-  return this.http.get<any[]>('http://localhost:3000/api/blogs'); //
-}
+    return this.http.get<any[]>(this.apiUrl);
+  }
 
-  // Get a single blog by ID
   getBlogById(id: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
