f047f51deb28b5374601ea2ea24b27228b7f767c
KibinCrew <mkevingalang@gmail.com>
2026-02-12 23:19:43 +0800

    Update login.ts

--- frontend/src/app/admin/login/login.ts
+++ frontend/src/app/admin/login/login.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router'; 
 import { AuthService } from '../../services/auth';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // Required for [(ngModel)]
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
