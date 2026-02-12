5337f53512908d60eee2bef61e5c9d4207b84196
KibinCrew <mkevingalang@gmail.com>
2026-02-12 23:16:53 +0800

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,17 +8,24 @@ import { LoginComponent } from './admin/login/login';
 import { DashboardComponent } from './admin/dashboard/dashboard';
 import { BlogDetailComponent } from './blogs/blog-detail/blog-detail';
 import { ShopDetail } from './shop/shop-detail/shop-detail';
+import { authGuard } from './admin/auth.guard';
 
 export const routes: Routes = [
-    { path : 'home', component: Home },
-    { path : 'shop', component: Shop },
-    { path : 'features', component: Features },
-    { path : 'contact', component: Contact },
+    { path: 'home', component: Home },
+    { path: 'shop', component: Shop },
+    { path: 'features', component: Features },
+    { path: 'contact', component: Contact },
     { path: 'blogs', component: BlogsComponent },
-    { path: 'admin/login', component: LoginComponent },
-    { path: 'admin/dashboard', component: DashboardComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'blogs/:id', component: BlogDetailComponent },
     { path: 'shop/:id', component: ShopDetail },
-    { path: '**', redirectTo: 'home' },
+    { path: 'admin/login', component: LoginComponent },
+    
+    { 
+      path: 'admin/dashboard', 
+      component: DashboardComponent, 
+      canActivate: [authGuard] 
+    }, 
+    
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' }
 ];
