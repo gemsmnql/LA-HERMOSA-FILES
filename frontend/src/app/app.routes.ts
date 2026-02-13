a4b4541b1231613af6d538466029004e747b4f96
KibinCrew <mkevingalang@gmail.com>
2026-02-13 17:24:00 +0800

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,8 @@ import { Features } from './features/features';
 import { Contact } from './contact/contact';
 import { BlogsComponent } from './blogs/blogs';
 import { Home } from './home/home';
-import { LoginComponent } from './admin/login/login';
-import { DashboardComponent } from './admin/dashboard/dashboard';
 import { BlogDetailComponent } from './blogs/blog-detail/blog-detail';
 import { ShopDetail } from './shop/shop-detail/shop-detail';
-import { authGuard } from './admin/auth.guard';
 
 export const routes: Routes = [
     { path: 'home', component: Home },
@@ -18,13 +15,6 @@ export const routes: Routes = [
     { path: 'blogs', component: BlogsComponent },
     { path: 'blogs/:id', component: BlogDetailComponent },
     { path: 'shop/:id', component: ShopDetail },
-    { path: 'admin/login', component: LoginComponent },
-    
-    { 
-      path: 'admin/dashboard', 
-      component: DashboardComponent, 
-      canActivate: [authGuard] 
-    }, 
     
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home' }
