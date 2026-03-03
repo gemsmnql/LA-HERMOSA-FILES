d511628752b77427f660883979fe54b6c2464d77
KibinCrew <mkevingalang@gmail.com>
2026-03-03 17:52:28 +0800

    Update blog.ts

--- frontend/src/app/services/blog.ts
+++ frontend/src/app/services/blog.ts
@@ -8,17 +8,42 @@ import { Observable } from 'rxjs';
 export class BlogService {
   private http = inject(HttpClient);
   
-  // Update this to your live Render URL
+  // Your live Render URL
   private apiUrl = 'https://la-hermosa-files.onrender.com/api/blogs';
 
   // Get all blogs
   getBlogs(): Observable<any[]> {
-    // Using the apiUrl variable here is cleaner
     return this.http.get<any[]>(this.apiUrl); 
   }
 
   // Get a single blog by ID
   getBlogById(id: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  /**
+   * Create a new blog post
+   * @param formData - The FormData object containing text and files
+   */
+  createBlog(formData: FormData): Observable<any> {
+    return this.http.post(this.apiUrl, formData);
+  }
+
+  /**
+   * Update an existing blog post
+   * @param id - The ID of the blog to update
+   * @param formData - The FormData object containing updated text and files
+   */
+  updateBlog(id: string, formData: FormData): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, formData);
+  }
+
+  /**
+   * Delete a blog post
+   * @param id - The ID of the blog to delete
+   */
+  deleteBlog(id: string): Observable<any> {
+    // Matching your server.js route: app.post('/api/blogs/delete/:id')
+    return this.http.post(`${this.apiUrl}/delete/${id}`, {});
   }
 }
