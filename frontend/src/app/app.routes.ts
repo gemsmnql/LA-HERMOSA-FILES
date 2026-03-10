6d03dd65369759d3ad241900dc4b69905dcba8ef
KibinCrew <mkevingalang@gmail.com>
2026-03-10 18:33:24 +0800

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,8 +4,11 @@ import { Features } from './features/features';
 import { Contact } from './contact/contact';
 import { BlogsComponent } from './blogs/blogs';
 import { Home } from './home/home';
+import { LoginComponent } from './admin/login/login';
+import { DashboardComponent } from './admin/dashboard/dashboard';
 import { BlogDetailComponent } from './blogs/blog-detail/blog-detail';
 import { ShopDetail } from './shop/shop-detail/shop-detail';
+import { authGuard } from './admin/auth.guard';
 
 export const routes: Routes = [
     { path: 'home', component: Home },
@@ -13,9 +16,18 @@ export const routes: Routes = [
     { path: 'features', component: Features },
     { path: 'contact', component: Contact },
     { path: 'blogs', component: BlogsComponent },
-    { path: 'blogs/:id', component: BlogDetailComponent },
+    { path: 'blogs/:slug', component: BlogDetailComponent },
     { path: 'shop/:id', component: ShopDetail },
+    { path: 'admin/login', component: LoginComponent },
     
+    // THE SECURE ROUTE
+    { 
+      path: 'admin/dashboard', 
+      component: DashboardComponent, 
+      canActivate: [authGuard] 
+    }, 
+    
+    // WILDCARDS (Must stay at the bottom)
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home' }
 ];
